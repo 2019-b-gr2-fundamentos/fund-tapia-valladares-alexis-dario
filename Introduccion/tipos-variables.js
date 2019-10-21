56d288f73ee6340870b9d70434b62d18dd94767f
tapiaalexv <tapiaalexv@gmail.com>
2019-10-21 15:01:34 -0500

    tipos variables js

---
+++ Introduccion/tipos-variables.js
@@ -0,0 +1,10 @@
+// Variables Primitivas
+const numero = 10
+const decimal = 10.12;
+const repuestaTipoNumero = typeof numero;
+console.log(repuestaTipoNumero)
+console.log(decimal);
+typeof numero
+typeof decimal
+
+//Gestor de paquetes de nodejs
