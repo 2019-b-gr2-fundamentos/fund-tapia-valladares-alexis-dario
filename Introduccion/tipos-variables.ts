5dcd79c808cfd057eb6c0b9b52953941bbf83e67
tapiaalexv <tapiaalexv@gmail.com>
2019-10-21 15:54:26 -0500

    ts

--- Introduccion/tipos-variables.ts
+++ Introduccion/tipos-variables.ts
@@ -14,6 +14,13 @@ console.log(typeof palabra);
 const hijos = null;
 console.log(typeof hijos)
 
+const fecha = new Date();
+console.log(typeof fecha)
+
+const casado = false;
+console.log(typeof casado)
+
+
 //Gestor de paquetes de nodejs 
 //Su funcion es comunicarse con el repositorio y descargar el "paquete"
 //Existen dos npm y yarn
