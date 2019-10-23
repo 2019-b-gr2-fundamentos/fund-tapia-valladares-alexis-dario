9cce5b5bcb24c1b1ea0ee9617ccd8a2a2ad83980
tapiaalexv <tapiaalexv@gmail.com>
2019-10-23 17:54:36 -0500

    operaciones

---
+++ Introduccion/03-numeros-strings.ts
@@ -0,0 +1,27 @@
+let numeroUno = 1; //camelCase
+const NUMERO_UNO =1;
+// NUMEROUNO -> TODO MAYUSCULA (SEPARACION NO)
+// NUMERO_UNO -> mayusculas con guion bajo
+// NumeroUno
+// numerouno
+// numero_uno -> PYTHON
+// numerOunO
+let resultado;
+
+//SUMA
+resultado = 1 + 2;
+//RESTA
+resultado = 1 - 2;
+//MULTIPLICAR
+resultado = 1 * 2;
+//DIVIDIR
+resultado = 1 / 2;
+//EXPONENCIAL
+resultado = 1 ^ 2;
+//MOD -> residuo de division
+resultado = 4 % 2; // = 0
+resultado = 4 % 3; // = 1
+resultado = 7 % 3; // = 1
+
+console.log("pera" + 2);
+
