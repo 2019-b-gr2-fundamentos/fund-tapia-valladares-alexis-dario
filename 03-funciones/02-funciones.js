2a2f0d4c5243948ac5e117c8b3ac60ece8d120eb
tapiaalexv <tapiaalexv@gmail.com>
2019-11-25 15:39:59 -0500

    funciones

---
+++ 03-funciones/02-funciones.js
@@ -0,0 +1,44 @@
+function sumar(numUno, numDos) {
+    return numUno + numDos;
+}
+function restar(numUno, numDos) {
+    return numUno - numDos;
+}
+function multiplicar(numUno, numDos) {
+    return numUno * numDos;
+}
+function dividir(numUno, numDos) {
+    return numUno / numDos;
+}
+function main() {
+    calculadora();
+}
+function calculadora() {
+    const operacion = prompt('Selecciona una operacion:\n1) Suma\n2) Resta\n3) Multiplicacion\n4) Division');
+    const esSuma = operacion == "suma" || operacion == "1" || operacion == "suma-1";
+    const esResta = operacion == "resta" || operacion == "2" || operacion == "resta-2";
+    const esMultiplicacion = operacion == "multiplicacion" || operacion == "3" || operacion == "multiplicacion-3";
+    const esDivision = operacion == "division" || operacion == "4" || operacion == "division-4";
+    const estaValida = esSuma || esResta || esDivision || esMultiplicacion;
+    if (estaValida) {
+        const numUno = +prompt("Numero 1: ");
+        const numDos = +prompt("Numero 2: ");
+        let resultado = 0;
+        if (esSuma) {
+            resultado = sumar(numUno, numDos);
+        }
+        if (esResta) {
+            resultado = restar(numUno, numDos);
+        }
+        if (esMultiplicacion) {
+            resultado = multiplicar(numUno, numDos);
+        }
+        if (esDivision) {
+            resultado = dividir(numUno, numDos);
+        }
+        console.log(resultado);
+    }
+    else {
+        calculadora();
+    }
+}
